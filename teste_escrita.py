06ac40d977a58bddc780ccd55e301adc965db94c
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-12 20:11:43 -0500

    Usando a+ e tentando alterar a segunda linha

--- teste_escrita.py
+++ teste_escrita.py
@@ -5,7 +5,7 @@ os.system("clear")
 # contato = "11,carol,carol@carol.com.br\n"
 # arquivo_contatos.write(contato)
 
-arquivo_contatos = open ("contatos-escrita.csv", encoding = "latin_1", mode = "w+")
+arquivo_contatos = open ("contatos-escrita.csv", encoding = "latin_1", mode = "a+")
 contatos = ["11,Carol,carol@carol.com.br\n",
             "12,Ana,ana@ana.com.br\n",
             "13,Tais,tais@tais.com.br\n",
