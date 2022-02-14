f78fc4cfd63e5c0231deb891950d65fad32c69b6
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-13 21:57:56 -0500

    Lendo e escrevendo no arquivo pickle

--- principal.py
+++ principal.py
@@ -1,14 +1,25 @@
 import os
-from contatos_utils import csv_para_contatos
+from contatos_utils import contatos_para_pickle, csv_para_contatos, pickle_para_contatos
 
 os.system("clear")
 
-try:
-    contatos = csv_para_contatos("contatos.csv")
-
+def imprime(contatos):
     for contato in contatos:
         print(f"{contato.id} - {contato.nome} - {contato.email}")
+    print()
+    print()
+
+try:
+    contatos = csv_para_contatos("contatos.csv")
+    imprime(contatos)
+
+    contatos_para_pickle(contatos, "contatos.pickle")
+
+    outro_contatos = pickle_para_contatos("contatos.pickle")
+    imprime(outro_contatos)
+
 except FileNotFoundError:
     print("Arquivo nao encontrado")
 except PermissionError:
     print("Permissao negada")
+
