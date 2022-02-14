b562ebb36c29548f4dbf4fb718b1cff31ce5ec52
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-13 20:27:06 -0500

    Usando with no teste_concorrencia

--- teste_concorrencia.py
+++ teste_concorrencia.py
@@ -1,11 +1,11 @@
-arquivo1 = open("contatos-escrita.csv", encoding = "latin_1", mode = "w")
-arquivo2 = open("contatos-escrita.csv", encoding = "latin_1", mode = "a")
+import os
+os.system('clear')
 
 contato_carol = "11,Carol,carol@carol.com.br\n"
 contato_andreza = "12,Andreza,andreza@andreza.com.br\n"
 
-arquivo1.write(contato_carol)
-arquivo2.write(contato_andreza)
+with open("contatos-escrita.csv", encoding = "latin_1", mode = "w") as arquivo1:
+    arquivo1.write(contato_carol)
 
-arquivo1.close()
-arquivo2.close()
+with open("contatos-escrita.csv", encoding = "latin_1", mode = "a") as arquivo2:
+    arquivo2.write(contato_andreza)
