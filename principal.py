956d7e84d420506db62d9c5c97278729e9381fae
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-13 20:07:44 -0500

    Excecao lancada ao abrir arquivo nao-existe.csv no modo leitura

--- principal.py
+++ principal.py
@@ -1,7 +1,9 @@
 import os
 os.system("clear")
 
-arquivo_contatos = open("contatos.csv", encoding = "latin_1")
+arquivo_contatos = open("nao-existe.csv", encoding = "latin_1")
 
 for linha in arquivo_contatos:
     print(linha,end = "")
+
+arquivo_contatos.close()
