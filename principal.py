da59d108ec6d5ce609cb54f2a3f865b6a8369015
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-13 20:08:53 -0500

    Arquivo e criado quando abre o arquivo no modo escrita

--- principal.py
+++ principal.py
@@ -1,7 +1,7 @@
 import os
 os.system("clear")
 
-arquivo_contatos = open("nao-existe.csv", encoding = "latin_1")
+arquivo_contatos = open("nao-existe.csv", encoding = "latin_1", mode = "w+")
 
 for linha in arquivo_contatos:
     print(linha,end = "")
