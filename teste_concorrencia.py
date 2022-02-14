4365f3a5ea37559d9b6672e8c584a206afc5df9e
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-13 19:51:54 -0500

    Colocando segundo como append

--- teste_concorrencia.py
+++ teste_concorrencia.py
@@ -1,5 +1,5 @@
 arquivo1 = open("contatos-escrita.csv", encoding = "latin_1", mode = "w")
-arquivo2 = open("contatos-escrita.csv", encoding = "latin_1", mode = "w")
+arquivo2 = open("contatos-escrita.csv", encoding = "latin_1", mode = "a")
 
 contato_carol = "11,Carol,carol@carol.com.br\n"
 contato_andreza = "12,Andreza,andreza@andreza.com.br\n"
